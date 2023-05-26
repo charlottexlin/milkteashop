44f86eec116a6eff12c3f7255f491924062fd087
cxl229 <cxl229@nyu.edu>
2023-05-26 11:17:20 -0400

    ingredient icon buttons now working to fill the cup - core game functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,57 @@ import * as ingredients from "./ingredients.js";
 
 function main() {
     // TODO: have a timer, run the game loop while the timer is above 0 seconds
+    activateIngredientButtons();
     const order = generateOrder();
     displayOrder(order);
+}
+
+// Makes all icons in the ingredient panel of the game clickable
+function activateIngredientButtons() {
+    ingredients.flavors.forEach((flavor) => {
+        const btn = document.getElementById(flavor.iconId);
+        btn.onclick = () => {fillTea(flavor)};
+    });
+    ingredients.toppings.forEach((topping) => {
+        const btn = document.getElementById(topping.iconId);
+        btn.onclick = () => {addTopping(topping)};
+    });
+    ingredients.temperatures.forEach((temp) => {
+        const btn = document.getElementById(temp.iconId);
+        btn.onclick = () => {addTemperature(temp)};
+    });
+}
+
+// Fills the cup with the given tea flavor
+function fillTea(flavor) {
+    const ele = document.getElementById("tea");
+    ele.src = flavor.img;
+    ele.alt = flavor.name;
+    deactivateButtons(ingredients.flavors);
+}
+
+// Adds the given topping to the cup
+function addTopping(topping) {
+    // TODO Toppings go on bottom except whipped cream
+    const ele = document.getElementById("topping");
+    ele.src = topping.img;
+    ele.alt = topping.name;
+    deactivateButtons(ingredients.toppings);
+}
+
+// Adds the given temperature to the cup
+function addTemperature(temp) {
+    const ele = document.getElementById("temperature");
+    ele.src = temp.img;
+    ele.alt = temp.name;
+    deactivateButtons(ingredients.temperatures);
+}
+
+// Removes onclick functions of all icons of the given array
+function deactivateButtons(elements) {
+    elements.forEach((element) => {
+        document.getElementById(element.iconId).onclick = null;
+    });
 }
 
 // Generates a random order consisting of a flavor, a topping, and a temperature
@@ -27,13 +76,13 @@ function rand(lower, upper) {
 function displayOrder(order) {
     const flavorOrder = document.getElementById("flavorOrder");
     flavorOrder.src = order.flavor.icon;
-    flavorOrder.alt = order.flavor.name;
+    flavorOrder.alt = order.flavor.name + " icon";
     const toppingOrder = document.getElementById("toppingOrder");
     toppingOrder.src = order.topping.icon;
-    toppingOrder.alt = order.topping.name;
+    toppingOrder.alt = order.topping.name + " icon";
     const tempOrder = document.getElementById("tempOrder");
     tempOrder.src = order.temp.icon;
-    tempOrder.alt = order.temp.name;
+    tempOrder.alt = order.temp.name + " icon";
 }
 
 // Call the main line of execution only when the DOM has completely loaded
--- src/ingredients.js
+++ src/ingredients.js
@@ -1,33 +1,33 @@
 const flavors = [
-    {name: "green tea", img: "./img/greenTea.png", icon: "./img/greenTeaIcon.png"},
-    {name: "strawberry", img: "./img/strawberryTea.png", icon: "./img/strawberryIcon.png"},
-    {name: "peach", img: "./img/peachTea.png", icon: "./img/peachIcon.png"},
-    {name: "taro", img: "./img/taroTea.png", icon: "./img/taroIcon.png"},
-    {name: "mango", img: "./img/mangoTea.png", icon: "./img/mangoIcon.png"},
-    {name: "lychee", img: "./img/lycheeTea.png", icon: "./img/lycheeIcon.png"},
-    {name: "melon", img: "./img/melonTea.png", icon: "./img/melonIcon.png"},
-    {name: "jasmine", img: "./img/jasmineTea.png", icon: "./img/jasmineIcon.png"},
+    {name: "green tea", img: "./img/greenTea.png", icon: "./img/greenTeaIcon.png", iconId: "greenTeaIcon"},
+    {name: "strawberry", img: "./img/strawberryTea.png", icon: "./img/strawberryIcon.png", iconId: "strawberryIcon"},
+    {name: "peach", img: "./img/peachTea.png", icon: "./img/peachIcon.png", iconId: "peachIcon"},
+    {name: "taro", img: "./img/taroTea.png", icon: "./img/taroIcon.png", iconId: "taroIcon"},
+    {name: "mango", img: "./img/mangoTea.png", icon: "./img/mangoIcon.png", iconId: "mangoIcon"},
+    {name: "lychee", img: "./img/lycheeTea.png", icon: "./img/lycheeIcon.png", iconId: "lycheeIcon"},
+    {name: "melon", img: "./img/melonTea.png", icon: "./img/melonIcon.png", iconId: "melonIcon"},
+    {name: "jasmine", img: "./img/jasmineTea.png", icon: "./img/jasmineIcon.png", iconId: "jasmineIcon"},
 ]
 
 const toppings = [
-    {name: "strawberry boba", img: "./img/strawberryBoba.png", icon: "./img/strawberryBobaIcon.png"},
-    {name: "mango boba", img: "./img/mangoBoba.png", icon: "./img/mangoBobaIcon.png"},
-    {name: "pearls", img: "./img/pearls.png", icon: "./img/pearlsIcon.png"},
-    {name: "red bean", img: "./img/redBean.png", icon: "./img/redBeanIcon.png"},
-    {name: "peach jelly", img: "./img/peachJelly.png", icon: "./img/peachJellyIcon.png"},
-    {name: "grass jelly", img: "./img/grassJelly.png", icon: "./img/grassJellyIcon.png"},
-    {name: "coffee jelly", img: "./img/coffeeJelly.png", icon: "./img/coffeeJellyIcon.png"},
-    {name: "whipped cream", img: "./img/whippedCream.png", icon: "./img/whippedCreamIcon.png"},
+    {name: "strawberry boba", img: "./img/strawberryBoba.png", icon: "./img/strawberryBobaIcon.png", iconId: "strawberryBobaIcon"},
+    {name: "mango boba", img: "./img/mangoBoba.png", icon: "./img/mangoBobaIcon.png", iconId: "mangoBobaIcon"},
+    {name: "pearls", img: "./img/pearls.png", icon: "./img/pearlsIcon.png", iconId: "pearlsIcon"},
+    {name: "red bean", img: "./img/redBean.png", icon: "./img/redBeanIcon.png", iconId: "redBeanIcon"},
+    {name: "peach jelly", img: "./img/peachJelly.png", icon: "./img/peachJellyIcon.png", iconId: "peachJellyIcon"},
+    {name: "grass jelly", img: "./img/grassJelly.png", icon: "./img/grassJellyIcon.png", iconId: "grassJellyIcon"},
+    {name: "coffee jelly", img: "./img/coffeeJelly.png", icon: "./img/coffeeJellyIcon.png", iconId: "coffeeJellyIcon"},
+    {name: "whipped cream", img: "./img/whippedCream.png", icon: "./img/whippedCreamIcon.png", iconId: "whippedCreamIcon"},
 ]
 
 const temperatures = [
-    {name: "cold", img: "./img/iceCubes.png", icon: "./img/coldIcon.png"},
-    {name: "room temp", img: "", icon: "./img/roomTempIcon.png"},
-    {name: "hot", img: "./img/steam.png", icon: "./img/hotIcon.png"},
+    {name: "cold", img: "./img/iceCubes.png", icon: "./img/coldIcon.png", iconId: "coldIcon"},
+    {name: "room temp", img: "", icon: "./img/roomTempIcon.png", iconId: "roomTempIcon"},
+    {name: "hot", img: "./img/steam.png", icon: "./img/hotIcon.png", iconId: "hotIcon"},
 ]
 
 export {
     flavors,
     toppings,
-    temperatures
+    temperatures,
 }
