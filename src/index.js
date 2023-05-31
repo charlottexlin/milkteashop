b06b4a6c7d0d2697ca41224edb79efdf9c77e53e
cxl229 <cxl229@nyu.edu>
2023-05-31 14:31:38 -0400

    added game loop (no timer yet) and money counter update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,92 @@
 import * as ingredients from "./ingredients.js";
 
-function main() {
+let money = 0;
+let currentTea = {flavor: "", topping: "", temperature: ""};
+
+// Call the main line of execution only when the DOM has completely loaded
+document.addEventListener('DOMContentLoaded', main);
+
+async function main() {
     // TODO: have a timer, run the game loop while the timer is above 0 seconds
-    activateIngredientButtons();
-    const order = generateOrder();
-    displayOrder(order);
+    while (true) {
+        const order = generateOrder();
+        setupButtons(order);
+        activateIngredientButtons();
+        displayOrder(order);
+        await doneBtnClicked();
+    }
+}
+
+// Generates a random order consisting of a flavor, a topping, and a temperature
+function generateOrder() {
+    const randFlav = ingredients.flavors[rand(0,8)];
+    const randTop = ingredients.toppings[rand(0,8)];
+    const randTemp = ingredients.temperatures[rand(0,3)];
+    return {
+        flavor: randFlav,
+        topping: randTop,
+        temperature: randTemp,
+    }
+}
+
+// Generates a random integer between the lower (inclusive) and upper bound (exclusive)
+function rand(lower, upper) {
+    return Math.floor(Math.random() * upper) + lower;
+}
+
+// Set up onclick functions for done and trash buttons
+function setupButtons(order) {
+    const doneBtn = document.getElementById("doneBtn");
+    doneBtn.onclick = () => {
+        submitOrder(currentTea, order);
+        // TODO possibly add animation here (finished cup slide out)
+        clearCup();
+        // TODO possibly add animation here (blank cup slide in)
+        activateIngredientButtons();
+    }
+    const trashBtn = document.getElementById("trashBtn");
+    trashBtn.onclick = () => {
+        clearCup();
+        activateIngredientButtons();
+    }
+}
+
+// Clear the cup (remove tea, toppings, and temperature)
+function clearCup() {
+    const tea = document.getElementById("tea");
+    tea.src = "";
+    tea.alt = "";
+    const topping = document.getElementById("topping");
+    topping.src = "";
+    topping.alt = "";
+    const temp = document.getElementById("temperature");
+    temp.src = "";
+    temp.alt = "";
+    currentTea = {flavor: "", topping: "", temperature: ""};
+}
+
+// Checks if the tea that the player made matches the ordered tea, then adds to player's money based on that
+function submitOrder(playerTea, orderTea) {
+    // Go through each of the keys in playerTea and count how many values match orderTea.
+    const correct = Object.keys(playerTea).reduce((accum, curr) => {
+        return playerTea[curr].name === orderTea[curr].name ? accum + 1 : accum;
+    }, 0);
+    if (correct === 3) { // 3 correct = perfect, give $3
+        money += 3;
+        updateMoneyCounter(money);
+    } else if (correct === 2) { // 2 correct = give $2
+        money += 2;
+        updateMoneyCounter(money);
+    } else if (correct === 1) { // 1 correct = give $1
+        money += 1;
+        updateMoneyCounter(money);
+    } // 0 correct = give $0
+    // TODO possibly do something here?
+}
+
+// Updates the money counter in the DOM to display the given value
+function updateMoneyCounter(value) {
+    document.getElementById("moneyCounter").innerHTML = "$" + value;
 }
 
 // Makes all icons in the ingredient panel of the game clickable
@@ -23,28 +105,34 @@ function activateIngredientButtons() {
     });
 }
 
-// Fills the cup with the given tea flavor
+// Fills the cup with the given tea flavor and deactivates flavor buttons
 function fillTea(flavor) {
     const ele = document.getElementById("tea");
     ele.src = flavor.img;
     ele.alt = flavor.name;
+    currentTea.flavor = flavor;
     deactivateButtons(ingredients.flavors);
 }
 
-// Adds the given topping to the cup
+// Adds the given topping to the cup and deactivates topping buttons
 function addTopping(topping) {
     // TODO Toppings go on bottom except whipped cream
+    // TODO also red bean and jelly have diff placements
     const ele = document.getElementById("topping");
     ele.src = topping.img;
     ele.alt = topping.name;
+    currentTea.topping = topping;
     deactivateButtons(ingredients.toppings);
 }
 
-// Adds the given temperature to the cup
+// Adds the given temperature to the cup and deactivates temperature buttons
 function addTemperature(temp) {
     const ele = document.getElementById("temperature");
-    ele.src = temp.img;
-    ele.alt = temp.name;
+    if (temp.name === "cold" || temp.name === "hot") {
+        ele.src = temp.img;
+        ele.alt = temp.name;
+    }
+    currentTea.temperature = temp;
     deactivateButtons(ingredients.temperatures);
 }
 
@@ -53,23 +141,6 @@ function deactivateButtons(elements) {
     elements.forEach((element) => {
         document.getElementById(element.iconId).onclick = null;
     });
-}
-
-// Generates a random order consisting of a flavor, a topping, and a temperature
-function generateOrder() {
-    const randFlav = ingredients.flavors[rand(0,8)];
-    const randTop = ingredients.toppings[rand(0,8)];
-    const randTemp = ingredients.temperatures[rand(0,3)];
-    return {
-        flavor: randFlav,
-        topping: randTop,
-        temp: randTemp,
-    }
-}
-
-// Generates a random integer between the lower (inclusive) and upper bound (exclusive)
-function rand(lower, upper) {
-    return Math.floor(Math.random() * upper) + lower;
 }
 
 // Displays the given order in the order panel of the game
@@ -81,9 +152,18 @@ function displayOrder(order) {
     toppingOrder.src = order.topping.icon;
     toppingOrder.alt = order.topping.name + " icon";
     const tempOrder = document.getElementById("tempOrder");
-    tempOrder.src = order.temp.icon;
-    tempOrder.alt = order.temp.name + " icon";
+    tempOrder.src = order.temperature.icon;
+    tempOrder.alt = order.temperature.name + " icon";
 }
 
-// Call the main line of execution only when the DOM has completely loaded
-document.addEventListener('DOMContentLoaded', main);
+function doneBtnClicked() {
+    // Code idea citation: https://stackoverflow.com/questions/54916739/wait-for-click-event-inside-a-for-loop-similar-to-prompt
+    return new Promise((resolve) => {
+        const doneBtn = document.getElementById("doneBtn");
+        function handleClick() {
+            doneBtn.removeEventListener('click', handleClick);
+            resolve();
+        }
+        doneBtn.addEventListener('click', handleClick);
+    });
+}
